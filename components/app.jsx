009635eb70793a2d86dd4d04eceaf7489215db05
Austin Fraser <austinbfraser@gmail.com>
2024-07-16 15:58:35 -0700

    added baseUrl back into sampler loader

--- components/app.jsx
+++ components/app.jsx
@@ -138,7 +138,7 @@ const App = () => {
       samplerRef.current = sampleFiles.map((file, index) => {
         return new Tone.Sampler({
           urls: { A1: file },
-          // baseUrl,
+          baseUrl,
           onload: () => console.log(`sampler${index + 1} loaded`),
         }).connect(gainNodeRef.current);
       });
